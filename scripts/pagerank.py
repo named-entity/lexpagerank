1882338436a5bf8ecba7ab575c6f9169077852df
named-entity <protoev@gmail.com>
2014-11-14 23:33:47 +0400

    связываем предложения без весов

--- scripts/pagerank.py
+++ scripts/pagerank.py
@@ -31,9 +31,10 @@ def build_sentence_graph(word, sentences):
         ts = [t.text for t in s]
         g.add_node(i, words=ts)
         for word in ts:
-            for n in g.nodes():
-                if w in n.words:
-                    g.add_edge(i, n)
+            for n in g.nodes(data=True):
+            # добавлять вес - количество таких слов
+                if word in n[1]['words']:
+                    g.add_edge(i, n[0])
     return g
 
 
@@ -61,11 +62,11 @@ def rank(g):
 
 if __name__ == '__main__':
     s = list(read_corpus(sys.stdin))
-    g = build_sentence_graph(s)
+    g = build_sentence_graph(u'лицо', s)
     pos = nx.spring_layout(g)
     nx.draw_networkx_nodes(g, pos)
     nx.draw_networkx_edges(g, pos)
-    nx.draw_networkx_labels(g, pos)
-    nx.draw_networkx_edge_labels(g, pos)
+#    nx.draw_networkx_labels(g, pos)
+#    nx.draw_networkx_edge_labels(g, pos)
     plt.show()
 #    print rank(g)
