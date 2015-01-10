ddbb44d075cf6ee99885b8113510d77b0118c43f
Ekaterina <protoev@gmail.com>
2015-01-10 23:59:40 +0300

    строим теперь граф для общих слов

--- scripts/pagerank.py
+++ scripts/pagerank.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 from collections import Counter, defaultdict
+from itertools import combinations
+from random import sample
 
 import matplotlib.pyplot as plt
 import matplotlib
 import networkx as nx
 import re
 import sys
-from networkx.algorithms.components.connected import number_connected_components
+from networkx.algorithms.components.connected import number_connected_components, connected_components
 from networkx.classes.multigraph import MultiGraph
-from utils import read_corpus
+from utils import read_lemmas
 
 
 class Scorer(object):
@@ -16,6 +18,40 @@ class Scorer(object):
         pass
 
     def __call__(self, *args, **kwargs):
+        pass
+
+
+class CGraph(MultiGraph):
+    def load(self, w, sentences):
+        # сюда будем для каждого общего слова писать номера предложений
+        wpairs = {}
+        ss = {}
+        f = open('%s.txt' % w, 'w')
+        for i, s in enumerate(sentences):
+            # лемматизация уже есть
+            if w in s:
+                print >> f, i, ' '.join(s).encode('utf-8')
+                ss[i] = ' '.join(s).encode('utf-8')
+                for w1, w2 in combinations(filter(lambda x: x != w and re.match(u'\w+', x, re.U), set(s)), 2):
+                    if w1 == w2:
+                        continue
+                    try:
+                        if i not in wpairs[(w1, w2)]:
+                            wpairs[(w1, w2)].append(i)
+                    except:
+                        wpairs[(w1, w2)] = [i]
+        for w1, w2 in wpairs.iterkeys():
+            if len(set(wpairs[(w1, w2)])) > 1:
+                self.add_edge(w1, w2)
+        print len(self.nodes())
+#                for j in set(wpairs[(w1, w2)]):
+#                    print w1.encode('utf-8'), w2.encode('utf-8'), j
+#                    self.add_edge(w1, w2, label=j, key=j, weight=1.0)
+        f.close()
+        return ss
+
+# тут будем удалять вершины по списку
+    def cut(self, common_words):
         pass
 
 
@@ -52,9 +88,9 @@ class SGraph(MultiGraph):
 
 def build_sentence_graph(w, sentences):
     # связываем предложения, в которых есть интересующее нас слово
-    g = SGraph()
-    words = g.load(w, sentences)
-    return g, words
+    g = CGraph()
+    sentences = g.load(w, sentences)
+    return g, sentences
 
 
 def rank(g):
@@ -66,22 +102,44 @@ def plot_graph(G):
     pos = nx.spring_layout(G)
     nx.draw_networkx_nodes(G, pos)
     nx.draw_networkx_edges(G, pos)
-    nx.draw_networkx_labels(G, pos)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=dict(zip(G.edges(), (d['label'] for (u, v, d) in G.edges(data=True)))),
-                                 label_pos=0.3)
+#    nx.draw_networkx_labels(G, pos)
+#    nx.draw_networkx_edge_labels(G, pos, edge_labels=dict(zip(G.edges(), (d['label'] for (u, v, d) in G.edges(data=True)))),
+#                                 label_pos=0.3)
     plt.show()
 
 
 if __name__ == '__main__':
     matplotlib.rc('font', **{'sans-serif': 'Arial',
                            'family': 'sans-serif'})
-    s = list(read_corpus(sys.stdin))
-    print len(s)
-    g, words = build_sentence_graph(u'замок', s)
+    # в функции build_sentence_graph можно заменить CGraph на SGraph
+    g, ss = build_sentence_graph(u'замок', read_lemmas(sys.stdin))
     print number_connected_components(g)
-    for w, c in sorted(words, key=lambda x: x[1], reverse=True)[:10]:
+    # тут удаление рёбер для SGraph'ов
+    """for w, c in sorted(words, key=lambda x: x[1], reverse=True)[:10]:
         for u, v in g.edges_iter():
             if g.has_edge(u, v, key=w):
                 g.remove_edge(u, v, key=w)
-    print number_connected_components(g)
-    plot_graph(g)
+    print number_connected_components(g)"""
+    # удалим вершины степени 1 и остальные сложим в словарь со степенями. нужно в 1 строчку
+    ndeg = {}
+    for n in g.nodes()[:]:
+        if g.degree(n) < 2:
+            print >> sys.stderr, n
+            g.remove_node(n)
+        else:
+            ndeg[n] = g.degree(n)
+    print len(ndeg)
+# рисуем распределение степеней
+#    plt.plot(range(len(ndeg)), sorted(ndeg.values(), reverse=True))
+#    plt.show()
+    # удаление вершин для CGraph'ов
+    for n, d in sorted(ndeg.iteritems(), key=lambda x: x[1], reverse=True)[:60]:
+        g.remove_node(n)
+        print n.encode('utf-8'), d, number_connected_components(g)
+    sg = connected_components(g)
+    # для десяти самых больших связных компонент выпишем предложения
+#    for c in sorted(sg, key=len, reverse=True)[:10]:
+#        cs = set((x[2]['label'] for x in g.edges(c, data=True)))
+#        for s in cs:
+#            print ss[s]
+#        print
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,7 +1,15 @@
 # coding: utf-8
+from pymorphy2 import MorphAnalyzer
 
 
-def read_corpus(fileobj):
+def read_lemmas(fileobj):
+    # здесь на каждой строчке по предложению (токенизованному)
+    m = MorphAnalyzer()
+    for line in fileobj:
+        yield [m.parse(t)[0].normal_form for t in line.decode('utf-8').split()]
+
+
+def read_corpus(fileobj, m=None):
     s = []
     for line in fileobj:
         line = line.rstrip('\n').decode('utf-8').split('\t')
@@ -13,14 +21,26 @@ def read_corpus(fileobj):
             continue
         if line[0] == '/sent':
             continue
-        s.append(Token(line))
+        s.append(Token(line, m))
 
 
 class Token():
-    def __init__(self, line):
-        self.text = line[1]
-        self.interp = line[2:]
-        self.lemmas = [i.split()[1] for i in self.interp]
+    def __init__(self, line, m=None):
+        if not m:
+            if len(line) < 2:
+                self.text = ''
+                self.lemmas = ['']
+            else:
+                self.text = line[1]
+                self.interp = line[2:]
+                self.lemmas = [i.split()[1] for i in self.interp]
+        else:
+            try:
+                self.text = line[1]
+                self.lemmas = [p.normal_form for p in m.parse(self.text)]
+            except:
+                self.text = ''
+                self.lemmas = ['']
 
     def add_interp(self):
         pass
@@ -29,3 +49,8 @@ class Token():
 class Sentence(list):
     def __init__(self):
         pass
+
+if __name__ == '__main__':
+    import sys
+    for s in read_corpus(sys.stdin):
+        print ' '.join((t.text for t in s)).encode('utf-8')
